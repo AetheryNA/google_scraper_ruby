4b59b8de6cf81977b7028f3e020c999ee9287008
AetheryNA <naveed@nimblehq.co>
2022-09-14 17:16:29 +0700

    [b002] Created new route for creating users and updated controller with handling registration flow

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -2,5 +2,22 @@ class RegisterController < ApplicationController
   layout 'auth'
 
   def index
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, Notice: "Successfully created Account"
+    else
+      render :index
+    end
+  end
+
+  private 
+
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end
 end
