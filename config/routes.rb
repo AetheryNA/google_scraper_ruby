1fcebd9e428edc5da467bdc3ec4f442acca9f297
AetheryNA <naveed@nimblehq.co>
2022-09-16 12:05:21 +0700

    [b001 - b002] Fix for failing CI for production build

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/home", to: "home#index"
 
-  # use_doorkeeper do
-  #   skip_controllers :authorizations, :applications, :authorized_applications
-  # end
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications, :authorized_applications
+  end
 
-  devise_for :users
+  unless defined?(::Rake::SprocketsTask)
+    devise_for :users
+  end
 
   # Defines the root path route ("/")
   root "home#index"
