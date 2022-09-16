e4e3278b1ec25bd9a2944f3691525c18cd38b1d3
AetheryNA <naveed@nimblehq.co>
2022-09-16 11:30:58 +0700

    [b001 - b002] Added controller and model along with a view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  use_doorkeeper do
-    skip_controllers :authorizations, :applications, :authorized_applications
-  end
-  
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   get "/home", to: "home#index"
+
+  # use_doorkeeper do
+  #   skip_controllers :authorizations, :applications, :authorized_applications
+  # end
+
+  devise_for :users
+
   # Defines the root path route ("/")
   root "home#index"
 end
