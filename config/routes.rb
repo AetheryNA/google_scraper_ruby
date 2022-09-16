79fcb1263f597450d693006766e7a5da913ae2fc
AetheryNA <naveed@nimblehq.co>
2022-09-16 11:11:27 +0700

    [b001 - b002] Created authentication and sessions using Devise, handles both sign in and sign up :sunglasses:

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications, :authorized_applications
+  end
+  
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/home", to: "home#index"
   # Defines the root path route ("/")
   root "home#index"
 end
