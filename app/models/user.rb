7c04c3e875b056748cae197cf0d7a9d3e6897b92
AetheryNA <naveed@nimblehq.co>
2022-09-22 16:40:28 +0700

    [b003] Updated models, routes and created new migration for keywords :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
 
+  has_many :keywords, dependent: :destroy
+
   # the authenticate method from devise documentation
   def self.authenticate(email, password)
     password_param = password
