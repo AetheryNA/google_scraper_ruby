d32bb263320e1633f242f39918a3f1aaf69c7c81
AetheryNA <naveed@nimblehq.co>
2022-09-26 15:29:19 +0700

    [B001 - B002] Comment fixes, removed class tags on SLIM template as it is required for the UI tickets, removed unused features from devise model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :validatable
 
   # the authenticate method from devise documentation
   def self.authenticate(email, password)
