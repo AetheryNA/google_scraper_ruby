c87b3f9e3f2230984ab07283fd27197b2f45df23
AetheryNA <naveed@nimblehq.co>
2022-10-06 16:40:43 +0700

    [#14] Search keywords background job

--- app/jobs/search_keyword_job.rb
+++ app/jobs/search_keyword_job.rb
@@ -7,9 +7,9 @@ class SearchKeywordJob < ApplicationJob
     keyword = Keyword.find(keyword_id)
 
     begin
-      GoogleSearchService.new(keyword: keyword.keyword).call
+      response = GoogleSearchService.new(keyword: keyword.keyword).call
 
-      keyword.update_status(:completed)
+      save_keyword_results(response, GoogleParseService.new(html: response))
     rescue ActiveRecord::RecordNotFound, ActiveRecord::StatementInvalid
       keyword.update_status(:failed)
     end
@@ -18,4 +18,10 @@ class SearchKeywordJob < ApplicationJob
   private
 
   attr_reader :keyword
+
+  def save_keyword_results(html, parse_service)
+    Keyword.transaction do
+      keyword.update_result(html: html, parse_service: parse_service, status: :completed)
+    end
+  end
 end
--- app/jobs/search_keywords_job.rb
+++ app/jobs/search_keywords_job.rb
@@ -4,8 +4,8 @@ class SearchKeywordsJob < ApplicationJob
   queue_as :default
 
   def perform(keyword_ids)
-    keyword_ids.each do |keyword_id|
-      SearchKeywordJob.set(wait: 3).perform_later(keyword_id)
+    keyword_ids.each_with_index do |keyword_id, index|
+      SearchKeywordJob.set(wait: index * 3).perform_later(keyword_id)
     end
   end
 end
