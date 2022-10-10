b11672b674bb54c433d7c5ac184032665ce0b5a8
AetheryNA <naveed@nimblehq.co>
2022-10-10 11:07:56 +0700

    [#14] Parsing google for data and saving the data to the database

--- app/jobs/search_keyword_job.rb
+++ app/jobs/search_keyword_job.rb
@@ -4,7 +4,7 @@ class SearchKeywordJob < ApplicationJob
   queue_as :default
 
   def perform(keyword_id)
-    keyword = Keyword.find(keyword_id)
+    @keyword = Keyword.find(keyword_id)
 
     begin
       response = GoogleSearchService.new(keyword: keyword.keyword).call
@@ -21,6 +21,9 @@ class SearchKeywordJob < ApplicationJob
 
   def save_keyword_results(html, parse_service)
     Keyword.transaction do
+      # rubocop:disable Rails::SkipsModelValidations
+      keyword.links.insert_all(parse_service.all_links)
+      # rubocop:enable Rails::SkipsModelValidations
       keyword.update_result(html: html, parse_service: parse_service, status: :completed)
     end
   end
