4a4350803280b4eeb0beefeeec5d594012655b0b
AetheryNA <naveed@nimblehq.co>
2022-10-10 22:42:32 +0700

    [#14] Fix for html response through http get for google page

--- app/services/google_search_service.rb
+++ app/services/google_search_service.rb
@@ -3,17 +3,17 @@
 class GoogleSearchService
   BASE_URL = 'https://www.google.com/search'
 
-  def initialize(keyword)
-    @url = "#{BASE_URL}?q=#{keyword}"
+  def initialize(keyword:)
+    @uri = URI("#{BASE_URL}?q=#{CGI.escape(keyword)}")
     @user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) '\
                   'Version/11.1.2 Safari/605.1.15'
   end
 
   def call
-    HTTParty.get(@url, { headers: { 'User-Agent' => user_agent } })
+    HTTParty.get(@uri, { headers: { 'User-Agent' => user_agent } })
   end
 
   private
 
-  attr_reader :url, :user_agent
+  attr_reader :uri, :user_agent
 end
